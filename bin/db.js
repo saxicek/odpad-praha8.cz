b76c6afeec73d8a9309e4f0fea82a0cb28813c4b
sax <saxicek@gmail.com>
2013-12-22 18:40:26 +0100

    Fixed error in DB initialization chain

--- bin/db.js
+++ bin/db.js
@@ -13,28 +13,12 @@ function createDBSchema(err, rows, result) {
   }
   // drop tables first
   // errors in this part are ignored - tables may and may not be present so DROP can fail
-  console.info('Dropping tables!');
+  console.info('Dropping table ODPAD');
   pg('DROP TABLE odpad;', function(err, rows, result){
-    if (err) {
-      console.error(error_response, err);
-    } else {
-      console.log('Table ODPAD dropped!');
-    }
+    console.info('Dropping table KNOWN_PLACES');
     return pg('DROP TABLE known_places;', function(err, rows, result){
-      if (err) {
-        console.error(error_response, err);
-      } else {
-        console.log('Table KNOWN_PLACES dropped!');
-      }
-      return pg('DROP TABLE odpad_import;', function(err, rows, result){
-        if (err) {
-          console.error(error_response, err);
-        } else {
-          console.log('Table ODPAD_IMPORT dropped!');
-        }
-        // follow up with create tables
-        return createOdpadTable;
-      });
+      console.info('Dropping table ODPAD_IMPORT');
+      return pg('DROP TABLE odpad_import;', createOdpadTable);
     });
   });
 }
