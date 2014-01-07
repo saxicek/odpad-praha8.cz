caec73fc0408ca37f505381c86533d41a0f9e11a
sax <saxicek@gmail.com>
2014-01-07 16:51:28 +0100

    Fixed error in cron script
    The error prevented daily automatic updates of data.

--- .openshift/cron/daily/update_containers.sh
+++ .openshift/cron/daily/update_containers.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -v http://$OPENSHIFT_NODEJS_IP:$OPENSHIFT_NODEJS_PORT/container/update >> "OPENSHIFT_NODEJS_LOG_DIR"container_update_`date +%Y-%m`.log 2>&1
+curl -v http://$OPENSHIFT_NODEJS_IP:$OPENSHIFT_NODEJS_PORT/container/update >> "$OPENSHIFT_NODEJS_LOG_DIR"container_update_`date +%Y-%m`.log 2>&1
