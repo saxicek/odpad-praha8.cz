cd71114abef1c9d219c0ee44bcf95f1e4d9ffd7d
sax <saxicek@gmail.com>
2014-08-23 20:42:52 +0200

    Updated unit tests for scraper

--- test/scraper.js
+++ test/scraper.js
@@ -1,29 +1,25 @@
 var expect  = require('chai').expect,
-    fs      = require('fs'),
     scraper = require('../bin/scraper.js');
 
 describe('scraper', function() {
 
-  describe('parseContainers()', function() {
-    var body;
-    // parse date is sensitive to current date since it is
-    // guessing year from current date and input parameters
-    before(function(done) {
-      fs.readFile('test/data/velkoobjemove_kontejnery_2014-04-30.html', 'utf8', function (err, data) {
-        if (err) {
-          return console.log(err);
-        }
-        body = data;
-        done();
-      });
-    });
-
+  describe('init()', function() {
     it('should expose a function', function () {
-      expect(scraper.parseContainers).to.be.a('function');
+      expect(scraper.init).to.be.a('function');
     });
 
-    it('should parse all rows', function() {
-      expect(scraper.parseContainers(body)).to.have.length(93);
+  });
+
+  describe('scrape()', function() {
+    it('should expose a function', function () {
+      expect(scraper.scrape).to.be.a('function');
+    });
+
+  });
+
+  describe('createScraper()', function() {
+    it('should expose a function', function () {
+      expect(scraper.createScraper).to.be.a('function');
     });
 
   });
