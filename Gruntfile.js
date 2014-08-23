7daa31f031bbc53caca2335af0ba20640c8b8c89
sax <saxicek@gmail.com>
2014-08-23 17:17:43 +0200

    Introduced Grunt for build and deploy

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+      options: {
+        baseUrl: 'src/js',
+        mainConfigFile: 'src/js/containers.js',
+        name: '../../bower_components/requirejs/require',
+        include: 'containers',
+        out: 'static/js/containers.js',
+        wrap: true
+      },
+      production: {
+      },
+      development: {
+        options: {
+          optimize: 'none'
+        }
+      }
+    },
+    migrate : {
+      up : "",
+      create: "",
+      down: "",
+      options: {
+        binaryPath: "migrate" //Path to the migrate module
+      }
+    },
+    watch: {
+      js: {
+        files: [
+          'work/js/**/*.js'
+        ],
+        tasks: [
+          'jshint',
+          'requirejs:development'
+        ]
+      }
+    },
+    jshint: {
+      server: [
+        'bin/**/*.js',
+        'config/*.js',
+        'migrations/*.js',
+        'test/**/*.js',
+        'Gruntfile.js',
+        'server.js'
+      ],
+      client: [
+        'src/js/**/*.js'
+      ]
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-migrate');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['requirejs:production']);
+  grunt.registerTask('deploy', ['migrate:up']);
+
+  // Default task(s).
+  grunt.registerTask('default', ['build', 'deploy']);
+};
