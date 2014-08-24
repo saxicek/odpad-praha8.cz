3a587bc57d4eca1651966cc161d8b81200177c8c
sax <saxicek@gmail.com>
2014-08-24 09:59:12 +0200

    Changed minimum scrape interval to one hour

--- config/default.js
+++ config/default.js
@@ -3,6 +3,6 @@ module.exports = {
   ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
   pg_config: process.env.OPENSHIFT_POSTGRESQL_DB_URL || 'postgresql://127.0.0.1:5432',
   schema_name: process.env.OPENSHIFT_APP_NAME || process.env.PG_MAP_TABLE_NAME || 'odpad',
-  scrape_interval: 24 * 60 * 60 * 1000, // daily (in milliseconds)
+  scrape_interval: 60 * 60 * 1000, // hourly (in milliseconds)
   db_inserts_parallel_limit: 5 // number of inserts into DB we want to run in parallel
 }
