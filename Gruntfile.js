50f872fd6c891f240af4cc4c548c085d6f474986
sax <saxicek@gmail.com>
2014-08-25 18:33:01 +0200

    Added more Grunt tasks to ease development
    Use `grunt dev` to start development. It starts Node server, restart it when sources are changed and runs unit tests and JSHint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,55 @@
+var config = require('config');
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    concurrent: {
+      dev: {
+        tasks: ['nodemon', 'node-inspector', 'watch'],
+        options: {
+          limit: 4,
+          logConcurrentOutput: true
+        }
+      }
+    },
+    nodemon: {
+      dev: {
+        script: 'server.js',
+        options: {
+          nodeArgs: ['--debug'],
+          env: {
+            PORT: config.port
+          },
+          // omit this property if you aren't serving HTML files and
+          // don't want to open a browser tab on start
+          callback: function (nodemon) {
+            nodemon.on('log', function (event) {
+              console.log(event.colour);
+            });
+
+            // refreshes browser when server reboots
+            nodemon.on('restart', function () {
+              // Delay before server listens on port
+              setTimeout(function() {
+                require('fs').writeFileSync('.rebooted', 'rebooted');
+              }, 1000);
+            });
+          },
+          watch: [
+            'templates/*.*',
+            'bin/**/*.js',
+            'config/*.js',
+            'test/**/*.js',
+            'server.js'
+          ]
+        }
+      }
+    },
+    'node-inspector': {
+      dev: {}
+    },
     requirejs: {
       options: {
         baseUrl: 'src/js',
@@ -12,9 +59,9 @@ module.exports = function(grunt) {
         out: 'static/js/containers.js',
         wrap: true
       },
-      production: {
+      prod: {
       },
-      development: {
+      dev: {
         options: {
           optimize: 'none'
         }
@@ -29,38 +76,61 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      js: {
+      livereload: {
+        options: {
+          livereload: true
+        },
         files: [
-          'bin/**/*.js',
-          'config/*.js',
-          'migrations/*.js',
-          'test/**/*.js',
-          '*.js',
-          'src/js/**/*.js'
+          '.rebooted',
+          'static/**/*.*'
+        ]
+      },
+      server: {
+        files: [
+          // watch only .rebooted - depend on nodemon to restart server
+          // whenever server files change
+          '.rebooted'
         ],
         tasks: [
           'mochaTest',
-          'jshint',
-          'requirejs:development'
+          'jshint:server'
         ]
+      },
+      client: {
+        files: [
+          'src/js/**/*.js'
+        ],
+        tasks: [
+          'jshint:client',
+          'requirejs:dev'
+        ]
+      },
+      configFiles: {
+        files: [ 'Gruntfile.js', 'config/*.js' ],
+        tasks: ['jshint:configFiles'],
+        options: {
+          reload: true
+        }
       }
     },
     jshint: {
       server: [
         'bin/**/*.js',
-        'config/*.js',
         'migrations/*.js',
         'test/**/*.js',
-        'Gruntfile.js',
         'server.js'
       ],
       client: [
         'src/js/**/*.js'
+      ],
+      configFiles: [
+        'Gruntfile.js',
+        'config/*.js'
       ]
     },
     // Configure a mochaTest task
     mochaTest: {
-      test: {
+      server: {
         options: {
           reporter: 'spec'
         },
@@ -75,9 +145,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-node-inspector');
 
-  grunt.registerTask('build', ['requirejs:production']);
+  grunt.registerTask('build', ['requirejs:prod']);
   grunt.registerTask('deploy', ['migrate:up']);
+  grunt.registerTask('dev', ['concurrent']);
 
   // Default task(s).
   grunt.registerTask('default', ['build', 'deploy']);
