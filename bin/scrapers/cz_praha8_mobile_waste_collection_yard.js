ad83d6557b25a1bafa965750b9d6dd78b23e05e1
sax <saxicek@gmail.com>
2014-10-03 18:44:42 +0200

    Added scraper for mobile waste collection yards

---
+++ bin/scrapers/cz_praha8_mobile_waste_collection_yard.js
@@ -0,0 +1,42 @@
+var
+  cheerio     = require('cheerio'),
+  util        = require('../util.js'),
+  path        = require('path'),
+  scraper     = require('../scraper.js').createScraper(path.basename(__filename, '.js'))
+;
+
+scraper.url = 'http://www.praha8.cz/Sberne-dvory';
+
+// district used for validation of container places
+scraper.districtName = 'Praha 8';
+
+// minimum interval between scrapes; format is moment.duration(String);
+scraper.minScrapeInterval = '1.00:00:00'; // once a day
+
+scraper.parse = function(body, callback) {
+  // parse html page
+  var containers = [],
+    $ = cheerio.load(body);
+
+  $('table.seda').find('tr:not(.sedaTableHeaderRow)').each(function(i) {
+    // skip header
+    var
+      cells = $(this).find('td'),
+      place_name = cells.eq(1).text().replace(/'/g,"''"),
+      raw_date = cells.eq(0).text(),
+      raw_time = cells.eq(2).text().trim() + '-' + cells.eq(3).text().trim(),
+      dates = util.parseDate(raw_date, raw_time);
+
+    containers[i] = {
+      place_name: place_name,
+      time_from: dates.time_from,
+      time_to: dates.time_to,
+      container_type: 'MOBILE_WASTE_COLLECTION_YARD'
+    };
+  });
+
+  // first argument of callback is null - no error was raised
+  callback(null, containers);
+};
+
+module.exports = exports = scraper;
---
+++ test/scrapers/cz_praha8_mobile_waste_collection_yard.js
@@ -0,0 +1,48 @@
+var
+  expect  = require('chai').expect,
+  fs      = require('fs'),
+  scraper = require('../../bin/scrapers/cz_praha8_mobile_waste_collection_yard.js');
+
+describe('cz_praha8_mobile_waste_collection_yard', function() {
+
+  describe('parse()', function() {
+    var body;
+    // parse date is sensitive to current date since it is
+    // guessing year from current date and input parameters
+    before(function(done) {
+      fs.readFile('test/data/mobilni_sberny_dvur_2014-10-03.html', 'utf8', function (err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        body = data;
+        done();
+      });
+    });
+
+    it('should expose a function', function () {
+      expect(scraper.parse).to.be.a('function');
+    });
+
+    it('should parse all rows', function(done) {
+      scraper.parse(body, function(err, res){
+        if (err) return done(err);
+        expect(res).to.have.length(2);
+        done();
+      });
+    });
+
+    it('should parse a row correctly', function(done) {
+      scraper.parse(body, function(err, res){
+        if (err) return done(err);
+        expect(res[0].place_name).to.equal('Zhořelecká x Radomská');
+        expect(res[0].time_from).to.eql(new Date((new Date()).getFullYear(), 9, 4, 8, 0));
+        expect(res[0].time_to).to.eql(new Date((new Date()).getFullYear(), 9, 4, 14, 0));
+        expect(res[0].container_type).to.equal('MOBILE_WASTE_COLLECTION_YARD');
+        done();
+      });
+    });
+
+  });
+
+});
+
