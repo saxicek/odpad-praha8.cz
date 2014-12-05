037bdad96f0cbf8030926e654720222bc729c55e
sax <saxicek@gmail.com>
2014-12-05 17:17:33 +0700

    Added scraper for electro waste containers in Prague 8

---
+++ bin/scrapers/cz_praha8_electro_waste.js
@@ -0,0 +1,40 @@
+var
+  cheerio     = require('cheerio'),
+  parserUtil  = require('../parser_util.js'),
+  path        = require('path'),
+  scraper     = require('../scraper.js').createScraper(path.basename(__filename, '.js'))
+;
+
+scraper.url = 'http://www.praha8.cz/Kontejnery-na-elektrozarizeni';
+
+// district used for validation of container places
+scraper.districtName = 'Praha 8';
+
+// minimum interval between scrapes; format is moment.duration(String);
+scraper.minScrapeInterval = '1.00:00:00'; // once a day
+
+// container type set to all parsed containers
+scraper.containerType = 'ELECTRO_WASTE';
+
+scraper.parse = function(body, callback) {
+  // parse html page
+  var containers = [],
+    $ = cheerio.load(body);
+
+  $('table.mcp8').find('tr:not(.mcp8TableHeaderRow)').each(function(i, elem) {
+    // skip header
+    var cells = $(this).find('td');
+    var place_name = cells.eq(2).text().trim().replace(/'/g,"''");
+
+    containers[i] = {
+      place_name: place_name,
+      time_from: null,
+      time_to: null
+    };
+  });
+
+  // first argument of callback is null - no error was raised
+  callback(null, containers);
+};
+
+module.exports = exports = scraper;
---
+++ test/scrapers/cz_praha8_electro_waste.js
@@ -0,0 +1,50 @@
+/* Disable Chai Expect jshint errors: https://github.com/chaijs/chai/issues/41 */
+/* jshint -W024 */
+/* jshint expr:true */
+var
+  expect  = require('chai').expect,
+  fs      = require('fs'),
+  scraper = require('../../bin/scrapers/cz_praha8_electro_waste.js');
+
+describe('cz_praha8_electro_waste', function() {
+
+  describe('parse()', function() {
+    var
+      body_1;
+    // parse date is sensitive to current date since it is
+    // guessing year from current date and input parameters
+    before(function(done) {
+      fs.readFile('test/data/elektro_odpad_2014-12-05.html', 'utf8', function (err, data) {
+        if (err) return console.log(err);
+        body_1 = data;
+        done();
+      });
+    });
+
+    it('should expose a function', function () {
+      expect(scraper.parse).to.be.a('function');
+    });
+
+    it('should parse all rows from first data set', function(done) {
+      scraper.parse(body_1, function(err, res){
+        if (err) return done(err);
+        expect(res).to.have.length(13);
+        done();
+      });
+    });
+
+    it('should parse row from first data correctly', function(done) {
+      scraper.parse(body_1, function(err, res){
+        if (err) return done(err);
+        expect(res[0].place_name).to.equal('KD  Ládví (Binarova)');
+        expect(res[0].time_from).to.be.null;
+        expect(res[0].time_to).to.be.null;
+        expect(res[0].container_type || scraper.containerType).to.equal('ELECTRO_WASTE');
+        done();
+      });
+    });
+
+  });
+
+});
+
