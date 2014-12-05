1ea97a41eaa77984006c35a6134e7aa26189ec5f
sax <saxicek@gmail.com>
2014-12-05 21:56:29 +0700

    Added SQL script that removes testing data from DB

---
+++ test/cleanup_test_data.sql
@@ -0,0 +1,11 @@
+﻿/*
+SELECT DISTINCT scraper_name FROM scrape_status;
+SELECT DISTINCT place_name FROM place ORDER BY place_name;
+SELECT DISTINCT district_name FROM district ORDER BY district_name;
+*/
+DELETE FROM scrape_status WHERE scraper_name LIKE 'test_scraper%' OR scraper_name IS NULL;
+DELETE FROM container
+ WHERE place_id IN (SELECT id FROM place WHERE place_name LIKE 'test_place%' OR place_name LIKE 'TEST_SCRAPE_PLACE%')
+    OR container_type LIKE 'TEST_CONTAINER_TYPE%';
+DELETE FROM place WHERE place_name LIKE 'test_place%' OR place_name LIKE 'TEST_SCRAPE_PLACE%';
+DELETE FROM district WHERE district_name LIKE 'TEST_SCRAPE_DISTRICT%';
