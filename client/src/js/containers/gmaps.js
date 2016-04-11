016462c61ca46c83fe894b8429db7b7eb1b06bb8
sax <saxicek@gmail.com>
2016-04-11 11:13:38 +0200

    Removed redundant parameter from Google Maps API

--- client/src/js/containers/gmaps.js
+++ client/src/js/containers/gmaps.js
@@ -1,6 +1,6 @@
 // convert Google Maps into an AMD module
 // http://blog.millermedeiros.com/requirejs-2-0-delayed-module-evaluation-and-google-maps/
-define('gmaps', ['async!http://maps.google.com/maps/api/js?v=3&sensor=false'],
+define('gmaps', ['async!http://maps.google.com/maps/api/js?v=3'],
   function(){
     // return the gmaps namespace for brevity
     return window.google.maps;
