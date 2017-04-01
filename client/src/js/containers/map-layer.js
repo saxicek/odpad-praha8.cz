c3d94e5f954aef7f81fb6b42624b45487305704d
sax <saxicek@gmail.com>
2017-04-01 07:28:05 +0200

    Removed MapQuest map layer since they stopped providing tiles

--- client/src/js/containers/map-layer.js
+++ client/src/js/containers/map-layer.js
@@ -8,13 +8,6 @@ define([
 
   var
 
-    // Map Quest OSM Layers
-    mapQuestOSM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
-      maxZoom:19,
-      subdomains:["otile1", "otile2", "otile3", "otile4"],
-      attribution:'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA.'
-    }),
-
     // Google Maps Layer
     googleRoad = new LeafGoogle('ROADMAP'),
 
@@ -33,7 +26,6 @@ define([
     }),
 
     baseLayers = {
-      "MapQuest OSM": mapQuestOSM,
       "Google": googleRoad,
       "Nokia": nokiaNormalDay,
       "Nokia satelitní": nokiaSatelliteLabelsDay
